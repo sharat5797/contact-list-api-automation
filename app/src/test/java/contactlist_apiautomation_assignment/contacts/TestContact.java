2dd4b6d20e78aed08d3dfa630944d8fa97b99f86
Sharat Hegde <sharath@testvagrant.com>
2023-10-18 15:32:35 +0530

    added negetive TestCases

--- app/src/test/java/contactlist_apiautomation_assignment/contacts/TestContact.java
+++ app/src/test/java/contactlist_apiautomation_assignment/contacts/TestContact.java
@@ -37,17 +37,17 @@ public class TestContact {
     @BeforeClass
     public void setUp() throws IOException {
         password = Utils.generatePassword();
-        firstName=Utils.generateFirstName();
-        lastName=Utils.generateLastName();
+        firstName = Utils.generateFirstName();
+        lastName = Utils.generateLastName();
 //        birthdate=Utils.generateBirthdate();
         email = Utils.generateEmail();
 //        phone=Utils.generatePhoneNumber();
-        street1=Utils.generateStreet1();
-        street2=Utils.generateStreet2();
-        city=Utils.generateCity();
-        stateProvince=Utils.generateStateProvince();
-        postalCode=Utils.generatePostalCode();
-        country=Utils.generateCountry();
+        street1 = Utils.generateStreet1();
+        street2 = Utils.generateStreet2();
+        city = Utils.generateCity();
+        stateProvince = Utils.generateStateProvince();
+        postalCode = Utils.generatePostalCode();
+        country = Utils.generateCountry();
 
         TestDataBuild testDataBuild = new TestDataBuild();
         CreateUserResponse createUserResponse = given().spec(Utils.requestSpecificationBuilder())
@@ -57,14 +57,15 @@ public class TestContact {
                 .extract().response().as(CreateUserResponse.class);
         token = createUserResponse.getToken();
     }
+
     @Test(priority = 0)
     public void shouldTestCreateContact() throws IOException {
         //Arrange
         ContactTestDataBuild contactTestDataBuild = new ContactTestDataBuild();
         //Act
         Contact contact = given().spec(Utils.requestSpecificationBuilder())
-                .header("Authorization",token)
-                .body(contactTestDataBuild.createContactPayload(firstName,lastName,"1970-01-01",email,"9876543210",street1,street2,city,stateProvince,postalCode,country))
+                .header("Authorization", token)
+                .body(contactTestDataBuild.createContactPayload(firstName, lastName, "1970-01-01", email, "9876543210", street1, street2, city, stateProvince, postalCode, country))
                 .when().post(APIResources.CreateContactAPI.getResource())
                 .then().spec(Utils.responseSpecificationBuilder()).assertThat().statusCode(201)
                 .extract().response().as(Contact.class);
@@ -83,9 +84,9 @@ public class TestContact {
                 .assertThat().statusCode(200)
                 .extract().response()
                 .jsonPath().getList(".", Contact.class);
-        id=contactList.get(0).getContact().get_id();
+        id = contactList.get(0).getContact().get_id();
         //Assert
-        Assert.assertEquals(contactList.size(),1);
+        Assert.assertEquals(contactList.size(), 1);
     }
 
     @Test(priority = 2)
@@ -94,22 +95,23 @@ public class TestContact {
         //Act
         Contact contact = given().spec(Utils.requestSpecificationBuilder())
                 .header("Authorization", "Bearer " + token)
-                .when().get(APIResources.GetContactAPI.getResource()+id)
+                .when().get(APIResources.GetContactAPI.getResource() + id)
                 .then().spec(Utils.responseSpecificationBuilder())
                 .assertThat().statusCode(200)
                 .extract().response().as(Contact.class);
         //Assert
-        Assert.assertEquals(contact.get_id(),id);
+        Assert.assertEquals(contact.get_id(), id);
     }
+
     @Test(priority = 3)
     public void shouldTestUpdateContact() throws IOException {
         //Arrange
         ContactTestDataBuild contactTestDataBuild = new ContactTestDataBuild();
         //Act
         Contact contact = given().spec(Utils.requestSpecificationBuilder())
-                .body(contactTestDataBuild.createContactPayload("HelloUser",lastName,"1970-01-01",email,"9876543210",street1,street2,city,stateProvince,postalCode,country))
+                .body(contactTestDataBuild.createContactPayload("HelloUser", lastName, "1970-01-01", email, "9876543210", street1, street2, city, stateProvince, postalCode, country))
                 .header("Authorization", "Bearer " + token)
-                .when().put(APIResources.UpdateContactAPI.getResource()+id)
+                .when().put(APIResources.UpdateContactAPI.getResource() + id)
                 .then().assertThat().statusCode(200)
                 .extract().response().as(Contact.class);
         //Assert
@@ -124,7 +126,7 @@ public class TestContact {
         Contact contact = given().spec(Utils.requestSpecificationBuilder())
                 .body(contactTestDataBuild.updateContactPayload("HelloUser2"))
                 .header("Authorization", "Bearer " + token)
-                .when().patch(APIResources.UpdateContactAPI.getResource()+id)
+                .when().patch(APIResources.UpdateContactAPI.getResource() + id)
                 .then().assertThat().statusCode(200)
                 .extract().response().as(Contact.class);
         //Assert
@@ -135,12 +137,93 @@ public class TestContact {
     public void shouldTestDeleteContact() throws IOException {
         //Arrange
         //Act
-        Response response = given().spec(Utils.requestSpecificationBuilder())
+        String response = given().spec(Utils.requestSpecificationBuilder())
                 .header("Authorization", "Bearer " + token)
-                .when().delete(APIResources.DeleteContactAPI.getResource()+id)
-                .then().extract().response();
+                .when().delete(APIResources.DeleteContactAPI.getResource() + id)
+                .then().extract().response().asString();
         //Assert
-        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response, "Contact deleted");
     }
 
+    //Negetive Cases
+    @Test(priority = 6)
+    public void shouldNotCreateContactTest() throws IOException {
+        //Arrange
+        ContactTestDataBuild contactTestDataBuild = new ContactTestDataBuild();
+        //Act
+        Response response = given().spec(Utils.requestSpecificationBuilder())
+                .header("Authorization", token)
+                .body(contactTestDataBuild.createContactPayload(firstName, lastName, "1970-01-01", email, "987654dhfgsudfygud3210", street1, street2, city, stateProvince, postalCode, country))
+                .when().post(APIResources.CreateContactAPI.getResource())
+                .then().spec(Utils.responseSpecificationBuilder())
+                .extract().response();
+        //Assert
+        response.then().assertThat().statusCode(400);
+    }
+
+    @Test(priority = 7)
+    public void shouldNotGetContactListTest() throws IOException {
+        //Arrange
+        //Act
+        Response response = given().spec(Utils.requestSpecificationBuilder())
+                .header("Authorization", "Bearer " + "jdhgfdusycgdsicstockernj")
+                .when().get(APIResources.GetContactListAPI.getResource())
+                .then().spec(Utils.responseSpecificationBuilder())
+                .extract().response();
+        //Assert
+        response.then().assertThat().statusCode(401);
+    }
+
+    @Test(priority = 8)
+    public void shouldNotGetContactTest() throws IOException {
+        //Arrange
+        //Act
+        Response response = given().spec(Utils.requestSpecificationBuilder())
+                .header("Authorization", "Bearer " + token)
+                .when().get(APIResources.GetContactAPI.getResource() + "jguy654134yruy7")
+                .then().extract().response();
+        //Assert
+        response.then().assertThat().statusCode(400);
+    }
+
+    @Test(priority = 9)
+    public void shouldNotUpdateContactTest() throws IOException {
+        //Arrange
+        ContactTestDataBuild contactTestDataBuild = new ContactTestDataBuild();
+        //Act
+        Response response = given().spec(Utils.requestSpecificationBuilder())
+                .body(contactTestDataBuild.createContactPayload("HelloUser", lastName, "1970-01-01", email, "987sdssdsjbsdhbsdcjnk6543210", street1, street2, city, stateProvince, postalCode, country))
+                .header("Authorization", "Bearer " + token)
+                .when().put(APIResources.UpdateContactAPI.getResource() + id)
+                .then()
+                .extract().response();
+        //Assert
+        response.then().assertThat().statusCode(400);
+    }
+
+    @Test(priority = 10)
+    public void shouldNotUpdateContactPatchTest() throws IOException {
+        //Arrange
+        ContactTestDataBuild contactTestDataBuild = new ContactTestDataBuild();
+        //Act
+        Response response = given().spec(Utils.requestSpecificationBuilder())
+                .body(contactTestDataBuild.updateContactPayload("HelloUser2"))
+                .header("Authorization", "Bearer " + token)
+                .when().patch(APIResources.UpdateContactAPI.getResource() + "dsjfhuis7438623")
+                .then()
+                .extract().response();
+        //Assert
+        response.then().assertThat().statusCode(400);
+    }
+    @Test(priority = 11)
+    public void shouldNotDeleteContactTest() throws IOException {
+        //Arrange
+        //Act
+        String response = given().spec(Utils.requestSpecificationBuilder())
+                .header("Authorization", "Bearer " + token)
+                .when().delete(APIResources.DeleteContactAPI.getResource() + "djcfhu8sdg632546718id")
+                .then().extract().response().asString();
+        //Assert
+        Assert.assertEquals(response, "Invalid Contact ID");
+    }
 }
